f77a1dd50cc645e76b9a5e7cdce79fd99236943b
EdwardCamiloQC <edward.camilo.q@gmail.com>
2022-02-22 14:56:57 -0500

    Se modificó el grosor de cada trazo utilizando la clase Graphics2D en vez de utilizar la pendiente de las coordenadas iniciales y finales de cada línea.

--- ProyectoLibreta/src/main/java/com/ed/proyectolibreta/Trazos.java
+++ ProyectoLibreta/src/main/java/com/ed/proyectolibreta/Trazos.java
@@ -4,19 +4,20 @@ import java.awt.Color;
 
 public class Trazos{
     
-    private int x1, x2, y1, y2;
+    private int x1, x2, y1, y2, grosorTrazo;
     Color color;
     
-    Trazos(int x1, int y1 ,int x2, int y2, Color c){
-        init(x1, y1, x2, y2, c);
+    Trazos(int x1, int y1 ,int x2, int y2, Color c, int grosor){
+        init(x1, y1, x2, y2, c, grosor);
     }
     
-    private void init(int x1, int y1, int x2, int y2, Color c){
+    private void init(int x1, int y1, int x2, int y2, Color c, int grosor){
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
         this.y2 = y2;
         color = c;
+        grosorTrazo = grosor;
     }
     
     public int getX1(){
@@ -37,5 +38,9 @@ public class Trazos{
     
     public Color getColorTrazo(){
         return color;
+    }
+    
+    public int getGrosor(){
+        return grosorTrazo;
     }
 }
